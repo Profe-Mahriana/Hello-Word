750a3851f960edaa3ba502c281bd1694e6d04683
Mariana <112775488+Profe-Mahriana@users.noreply.github.com>
2024-05-12 18:26:56 -0300

    Add JavaScript

---
+++ main.js
@@ -0,0 +1,11 @@
+let nomeUsuário = "";
+let elemento= document.querySelector("#nomeUsuario");
+
+
+while (nomeUsuário=="") {
+    nomeUsuário = prompt("Qual seu nome?");   
+}
+
+if (nomeUsuário==null) {
+    elemento.textContent = "Seja muito bem-vindo"
+} else{elemento.textContent= nomeUsuário;}
